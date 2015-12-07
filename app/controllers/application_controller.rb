4d1b73a9e572460b9cf759dcf5aed2a21e922d28
Anson Li <ansonli.94@gmail.com>
2015-12-07 14:43:30 -0700

    FAVICON UPDATE

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+# encoding: utf-8
+require 'riotgames_api.rb'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def hello 
+    RiotGamesAPI.set_region('NA')
+    render text: "hello" 
+  end
+  
+
 end
---
+++ app/helpers/map_results_helper.rb
@@ -0,0 +1,2 @@
+module MapResultsHelper
+end
